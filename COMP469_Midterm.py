5358bc736025d3bf88d5d7ce53b199c8b3ca05ac
TristanNibbe96 <60871491+TristanNibbe96@users.noreply.github.com>
2020-10-22 20:51:10 -0700

    Finished
    If you wanna test it out feel free, I tried a couple different versions of the board and they all seemed to work. Let me know if there are any issues.

--- COMP469_Midterm.py
+++ COMP469_Midterm.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 22 13:24:27 2020
-
 @author: Tristan Nibbe and Luis Figueroa
 """
-import random
+
+def printPath(path):
+  for i in range(len(path)-1,-1,-1):
+    print("->" + str(path[i]),end="")
+
+class MoveNode:
+
+  def __init__(self,parent,location,direction):
+    self.parent = parent
+    self.location = location
+    self.direction = direction
+
 class RobotMaze():
-    
+
     class Location():
         
         def __init__(self,x_coordinate= None,y_coordinate=None):
@@ -43,7 +53,7 @@ class RobotMaze():
         stop_reason = ""
         
         if(direction =="down"):
-            print("moving down")
+            #print("moving down")
             for i in range(current_location.x,len(self.board)):
                 if(temp[i][current_location.y]=="D"):
                     stop_reason="goal"
@@ -56,7 +66,7 @@ class RobotMaze():
                     
         elif(direction=="up"):
             
-            print("moving up")
+            #print("moving up")
             for i in range(current_location.x,-1,-1):
                 
                 if(temp[i][current_location.y]=="D"):
@@ -71,7 +81,7 @@ class RobotMaze():
            
         elif(direction =="right"):
             
-            print("moving right")
+            #print("moving right")
             for i in range(current_location.y,len(self.board)):
                 
                 if(temp[current_location.x][i]=="D"):
@@ -84,7 +94,7 @@ class RobotMaze():
                     break
                             
         else:
-            print("moving left")
+            #print("moving left")
             for i in range(current_location.y,-1,-1):
                 if(temp[current_location.x][i]=="D"):
                     stop_reason="goal"
@@ -97,35 +107,70 @@ class RobotMaze():
                 
                 
         return (stop_reason,stop_location)
-            
-    
+         
+	#creates a node to represent a movement choice, takes the direction that was made to get here and the node that that movement was made from
+    def createMoveNode(self,parent,direction):
+      newMove = self.move_direction(direction,parent.location)
+      newNode = MoveNode(parent,newMove[1],direction)
+      return newNode
+
+	#tests if two locations are equal
+    def areLocsEqual(self,locationOne,locationTwo):
+      if locationOne.x == locationTwo.x and locationOne.y == locationTwo.y:
+        return True
+      else:
+        return False
+
+	#creates a move node for up down left and right of the entered node, removing any that end up being equal to the parent because this means that is not a valid move
+    def createMoveNodeList(self,parent):
+        leftNode = self.createMoveNode(parent,"left")
+        rightNode = self.createMoveNode(parent,"right")
+        upNode = self.createMoveNode(parent,"up")
+        downNode = self.createMoveNode(parent,"down")
+
+        moveList = [leftNode,rightNode,upNode,downNode]
+
+        for node in moveList:
+          if(self.areLocsEqual(node.location,parent.location) == True):
+            moveList.remove(node)
+        return moveList
+	
+	#takes a node, returns the path that the start node took to reach that node
+    def returnPath(self,endNode):
+      currNode = endNode
+      path = []
+      while currNode.parent != None:
+        path.append(currNode.direction)
+        currNode = currNode.parent
+      return path
+
     def find_solution(self):
+        goal_loc = self.find_location_of("D")
+        currNode = MoveNode(None,self.find_location_of("R"),None)
+        moveList = []
         
-        
-        robot_location = self.find_location_of("R")
-        print(f"Location of robot X:{robot_location.x} Y:{robot_location.y}")
-        print(f"Goal X:{self.goal_location.x} Y:{self.goal_location.y}")
-       
-        (reason, stop_loc )= self.move_direction("left", robot_location)
-        print(f"stop @ X:{stop_loc.x} Y:{stop_loc.y}  Reason:{reason}\n")
-            
-        (reason, stop_loc )= self.move_direction("down", robot_location)
-        print(f"stop @ X:{stop_loc.x} Y:{stop_loc.y}  Reason:{reason}\n")
+        while(self.areLocsEqual(currNode.location,goal_loc) == False):
+          newMoveList = self.createMoveNodeList(currNode)
+          for node in newMoveList:
+            moveList.append(node)
 
+          currNode = moveList.pop(0)
 
-        pass
+        path = self.returnPath(currNode)
+        return(path)
     
     
-board = [['1','1','1','1','1','1','1','1','1'],
+board =     [['1','1','1','1','1','1','1','1','1'],
              ['1','0','0','0','1','0','0','0','1'],  
              ['1','0','0','0','1','0','0','R','1'],
              ['1','0','0','0','0','0','0','0','1'],
              ['1','0','0','0','0','1','0','0','1'],
-             ['1','1','1','0','0','1','0','D','1'],
+             ['1','1','1','0','0','1','0','0','1'],
              ['1','1','1','0','0','1','1','1','1'],
              ['1','1','0','0','0','0','0','D','1'],
              ['1','1','1','1','1','1','1','1','1']]
 
 
 myRobotMaze = RobotMaze(board)
-myRobotMaze.find_solution()
+path = myRobotMaze.find_solution()
+printPath(path)
